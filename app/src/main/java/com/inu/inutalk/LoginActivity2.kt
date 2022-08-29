912ab4863b82af9f5b9cec9644535bbb1eb0ba4a
inush <inushongg@gmail.com>
2022-08-29 15:57:05 +0900

    초기

---
+++ app/src/main/java/com/inu/inutalk/LoginActivity2.kt
@@ -0,0 +1,11 @@
+package com.inu.inutalk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity2: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login2)
+    }
+}
